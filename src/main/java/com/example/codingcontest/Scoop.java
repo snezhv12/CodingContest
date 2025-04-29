2f7fba2bb09dd9d0bf33854ed5c7a1eff691930c
snezhv <snizhana.pashkevych@students.fhnw.ch>
2025-04-29 14:49:24 +0200

    5th commit

--- src/main/java/com/example/codingcontest/Scoop.java
+++ src/main/java/com/example/codingcontest/Scoop.java
@@ -7,17 +7,3 @@ public class Scoop {
         this.flavor = flavor;
     }
 
-    public Flavor getFlavor() {
-        return flavor;
-    }
-
-    public double getGrossPrice() {
-        return flavor.getGrossPrice();
-    }
-
-    @Override
-    public String toString() {
-        return flavor.getName() + " (€" + String.format("%.2f", getGrossPrice()) + ")";
-    }
-}
-sdasdadadsa
